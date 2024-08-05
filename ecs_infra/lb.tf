bc8dab3ee163c448d420139eb0b788bf206858c4
alexis.metwalli <alexis.metwalli@devoteam.com>
2024-08-05 16:45:29 +0200

    Fix the ALB problem and remove public IP access

--- ecs_infra/lb.tf
+++ ecs_infra/lb.tf
@@ -1,9 +1,9 @@
 # Load Balancer for the Dagster webserver
 resource "aws_lb" "ecs_lb" {
   name               = "${var.app_name}-lb"
-  internal           = true
+  internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.webserver_sg.id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 }
 
@@ -16,6 +16,7 @@ resource "aws_lb_target_group" "ecs_lb" {
   health_check {
     path                = "/"
     protocol            = "HTTP"
+    matcher             = "200"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
@@ -25,14 +26,11 @@ resource "aws_lb_target_group" "ecs_lb" {
 
 resource "aws_lb_listener" "ecs_lb" {
   load_balancer_arn = aws_lb.ecs_lb.arn
-  port              = "3000"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ecs_lb.arn
-  }
-  lifecycle {
-    create_before_destroy = true
   }
 }
--- ecs_infra/security_groups.tf
+++ ecs_infra/security_groups.tf
@@ -1,18 +1,19 @@
 resource "aws_security_group" "webserver_sg" {
   vpc_id = aws_vpc.main.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id] # Allow traffic from the ALB security group
   }
 
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
@@ -20,13 +21,13 @@ resource "aws_security_group" "daemon_sg" {
   vpc_id = aws_vpc.main.id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
-  # Allow traffic from webserver and user code
   ingress {
     from_port   = 0
     to_port     = 0
@@ -39,10 +40,11 @@ resource "aws_security_group" "usercode_sg" {
   vpc_id = aws_vpc.main.id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   # Allow traffic from webserver and daemon
@@ -59,10 +61,11 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = aws_vpc.main.id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -70,5 +73,26 @@ resource "aws_security_group" "rds_sg" {
     to_port         = 5432
     protocol        = "tcp"
     security_groups = [aws_security_group.webserver_sg.id, aws_security_group.daemon_sg.id]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.app_name}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
